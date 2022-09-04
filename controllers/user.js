754af07234ed52e78f7af250133d4eb8bcfcd0cb
Sedov Dmitry <SedoyDi@yandex.ru>
2022-09-04 14:50:41 +0300

    fix: createUser controller

--- controllers/user.js
+++ controllers/user.js
@@ -42,7 +42,7 @@ const createUser = (req, res) => {
     .then((user) => {
       res.send(user);
     })
-    .catch((err) => {
+    .catch(({name: err}) => {
       if (err === "ValidationError") {
         res.status(INCORRECT_DATA).send({ message: "Некорректные данные" });
       } else {
