b36b59d1e1254f805e20baf3eb50943a4eb605b9
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2022-09-07 01:47:27 +0300

    fix: review bags 5.0

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
 const cardRoutes = require('./routes/card');
-const { NOT_FOUND } = require('./errors/errors_code');
+const { NOT_FOUND } = require('./status/status_code');
 
 const { PORT = 3000 } = process.env;
 
