c7551afed4ffe2a5262f9f33affdd50bc417303c
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2022-10-08 23:13:39 +0300

    fix: app.js

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
-
+const cors = require('cors');
+const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { login, createUser } = require('./controllers/user');
 const auth = require('./middlewares/auth');
-require('dotenv').config();
-
 const NotFoundError = require('./errors/notFoudError');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => console.log('connected'))
+  .catch((err) => console.log(`Ошибка ${err.name}: ${err.message}`));
+
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.path} ${JSON.stringify(req.body)}`);
+  next();
+});
 
 app.post('/signin', login);
 app.post('/signup', createUser);
@@ -23,6 +33,8 @@ app.use('/cards', require('./routes/card'));
 app.use('/*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
